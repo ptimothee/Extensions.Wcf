055b4f58ca3a630a6516179717c37288afb4afb8
Patrick Timothee <ptimothee@incomm.com>
2022-10-05 23:22:32 -0400

    Added test project

---
+++ tests/Fixtures/ISampleTestService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace Codemancer.Extensions.Wcf.Tests.Fixtures
+{
+    [ServiceContract]
+    public interface ISampleTestService
+    {
+        [OperationContract]
+        string Example1(int param1, string param2, bool param3);
+
+        [OperationContract]
+        Task<string> Example1Async(int param1, string param2, bool param3);
+    }
+}
---
+++ tests/PolicyHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+using Polly;
+using FluentAssertions;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Codemancer.Extensions.DependencyInjection;
+using Codemancer.Extensions.Wcf.Tests.Fixtures;
+
+namespace Codemancer.Extensions.Wcf.Tests
+{
+    public class PolicyHandlerTests
+    {
+        private readonly IHostBuilder _hostBuilder;
+
+        public PolicyHandlerTests()
+        {
+            _hostBuilder = Host.CreateDefaultBuilder()
+                                .UseDefaultServiceProvider(options =>
+                                {
+                                    options.ValidateOnBuild = true;
+                                    options.ValidateScopes = true;
+                                })
+                                .UseEnvironment(Environments.Development);
+        }
+
+        [Theory]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "Integration")]
+        [InlineData(1)]
+        public void WhenConfigureRetryPolicyOnSyncOperationShouldApplyRetryCount(int expectedRetry)
+        {
+            int actualRetry = 0;
+            using var host = _hostBuilder.ConfigureServices(services =>
+            {
+                var policy = Policy<Message>.Handle<EndpointNotFoundException>()
+                                            .RetryAsync(expectedRetry, (resp, count) =>
+                                            {
+                                                actualRetry++;
+                                            });
+
+                services.AddWcf<ISampleTestService>()
+                        .ConfigureServiceEndpoint(endpoint =>
+                        {
+                            endpoint.Address = new EndpointAddress("http://localhost");
+                        })
+                        .AddPolicyHandler(policy);
+
+            }).Build();
+
+            var service = host.Services.GetRequiredService<ISampleTestService>();
+
+            Action serviceOperation = () => service.Example1(0, "", true);
+
+            serviceOperation.Should().Throw<EndpointNotFoundException>();
+
+            actualRetry.Should().Be(expectedRetry);
+        }
+
+        [Theory]
+        [Trait("Category", "Functional")]
+        [Trait("Category", "Integration")]
+        [InlineData(1)]
+        public async Task WhenConfigureRetryPolicyOnAsyncOperationShouldApplyRetryCount(int expectedRetry)
+        {
+            int actualRetry = 0;
+            using var host = _hostBuilder.ConfigureServices(services =>
+            {
+                var policy = Policy<Message>.Handle<EndpointNotFoundException>()
+                                            .RetryAsync(expectedRetry, (resp, count) =>
+                                            {
+                                                actualRetry++;
+                                            });
+
+                services.AddWcf<ISampleTestService>()
+                        .ConfigureServiceEndpoint(endpoint =>
+                        {
+                            endpoint.Address = new EndpointAddress("http://localhost");
+                        })
+                        .AddPolicyHandler(policy);
+
+            }).Build();
+
+            var service = host.Services.GetRequiredService<ISampleTestService>();
+
+            Func<Task> serviceOperation = () => service.Example1Async(0, "", true);
+
+            await serviceOperation.Should().ThrowAsync<EndpointNotFoundException>();
+
+            actualRetry.Should().Be(expectedRetry);
+        }
+    }
+}
